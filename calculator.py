d5f5550d38036a44803104669e5660729b4ed22d
vagrant <vagrant@vagrant.vm>
2020-07-08 23:34:59 +0000

    Final Code

--- calculator.py
+++ calculator.py
@@ -4,43 +4,49 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-def calculator():
 """Creating a function for a calculator"""
 
-    while True:
-        input_string = input("Your equation?")
-        tokens = input_string.split(' ')
-        symbol = tokens[0]
+while True:
+    input_string = input("Your equation?")
+    tokens = input_string.split(' ')
 
-        if tokens[0] == "q":
-            print("Calculator exiting.")
-            break
+    if "q" in tokens:
+        print("Calculator exiting.")
+        break
 
-        if symbol == '+':
-            answer = add(num1, num2)
+    symbol = tokens[0]
+    num1 = tokens[1]
 
-        elif symbol == '-':
-            answer = subtract(num1, num2)
+    if len(tokens) == 3:
+        num2 = tokens[2]
 
-        elif symbol == '*': 
-            answer = multiply(num1, num2)
+    if len(tokens) > 3:
+        num3 = tokens[3]
 
-        elif symbol == '/': 
-            answer = divide(num1, num2)
+    answer = None
 
-        elif symbol == '**': 
-            answer = square(num1, num2)
+    if symbol == '+':
+        answer = add(int(num1), int(num2))
 
-        elif symbol == '*3': 
-            answer = cube(num1, num2)
+    elif symbol == '-':
+        answer = subtract(int(num1), int(num2))
 
-        elif symbol == '**': 
-            answer = power(num1, num2)
+    elif symbol == '*': 
+        answer = multiply(int(num1), int(num2))
 
-        elif symbol == '%': 
-            answer = mod(num1, num2)
+    elif symbol == '/': 
+        answer = divide(int(num1), int(num2))
 
-return answer
+    elif symbol == 'square': 
+        answer = square(int(num1))
 
+    elif symbol == 'cube': 
+        answer = cube(int(num1))
 
-calculator()
+    elif symbol == 'pow': 
+        answer = power(int(num1), int(num2))
+
+    elif symbol == 'mod': 
+        answer = mod(int(num1), int(num2))
+
+    print(answer)
