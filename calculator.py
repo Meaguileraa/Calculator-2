36fa07443fdda68d52de0d1e55de52b146dbffb9
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-08 16:05:45 -0700

    Addition added

--- calculator.py
+++ calculator.py
@@ -4,4 +4,21 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
+def calculator():
 
+    while True:
+        input_string = input("Your equation?")
+        tokens = input_string.split(' ')
+        symbol = tokens[0]
+
+        if tokens[0] == "q":
+            print("Calculator exiting.")
+            break
+
+        if symbol == '+':
+            answer = add(num1, num2)
+
+return answer
+
+
+calculator()
